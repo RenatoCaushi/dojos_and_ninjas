cd744edacb2dad68537a8b9314645ef4a052f3e4
RenatoCaushi <ren.caushi22@gmail.com>
2022-11-29 22:44:06 +0100

    updates

--- flask_app/models/dojo.py
+++ flask_app/models/dojo.py
@@ -1,5 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-from flask_app.models.ninja import Ninja
+
 
 class Dojo:
     def __init__(self, data):
--- flask_app/models/ninja.py
+++ flask_app/models/ninja.py
@@ -1,5 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-from flask_app.models.dojo import Dojo
+
 
 class Ninja:
     def __init__(self, data):
